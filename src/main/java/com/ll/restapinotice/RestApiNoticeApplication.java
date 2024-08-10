2c745d4a5f6f89a2aa2fbfca6c6abc44c44985af
sign0329 <sangminkim@gmail.com>
2024-08-10 14:45:32 +0900

    초기 설정

---
+++ src/main/java/com/ll/restapinotice/RestApiNoticeApplication.java
@@ -0,0 +1,13 @@
+package com.ll.restapinotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiNoticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiNoticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/restapinotice/RestApiNoticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.restapinotice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiNoticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
